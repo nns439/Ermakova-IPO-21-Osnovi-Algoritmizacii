2f00251013d76f41ea3b19718249c4a47d6b8c13
nns439 <nanse7nns@gmail.com>
2025-11-07 11:21:16 +0300

    Add files via upload

---
+++ Изучение_переменных_и_констант_литералов_типов_данных_консольный _ввод_вывод/calculator/calculator1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{ 
+    static void Main()
+    {
+        Console.WriteLine("Введитепервое число:");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введитевторое число:");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Сложение: " + (a + b));
+        Console.WriteLine("Вычитание: " + (a - b));
+        Console.WriteLine("Умножение: " + (a * b));
+
+        if (b != 0)
+        {
+            Console.WriteLine("Деление: " + (a / b));
+            Console.WriteLine("Остатокот деления: " + (a % b));
+        }
+        else
+        {
+        Console.WriteLine("Нельзяделить на ноль!");
+        }
+
+        a = a + 1;
+        b = b + 1;
+        Console.WriteLine("Инкремент: " + a + " и " + b);
+
+        a = a - 1;
+        b = b - 1;
+        Console.WriteLine("Декремент: " + a + " и " + b);
+        Console.WriteLine("Число 'a' > Числа 'b' и Число'b' > 0?" + ((a > b) && (b > 0)));
+        Console.WriteLine("Число 'a' > Числа 'b' или Число 'b' > Числа 'a'?" + ((a > b) || (b > a)));
+
+    }
+
+ }
